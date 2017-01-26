29c98293c777c9078bbce351dff9d6cb9b50599f
sg0993 <sg0993@163.com>
2017-01-26 09:34:25 +0800

    增加butterknife 插件使用,zai dial fragment 测试Ok！！！！！！！！！

--- app/src/main/java/com/honeywell/homepanel/fragment/DialFragment.java
+++ app/src/main/java/com/honeywell/homepanel/fragment/DialFragment.java
@@ -6,8 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.honeywell.homepanel.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by H135901 on 1/25/2017.
@@ -15,17 +19,29 @@ import com.honeywell.homepanel.R;
 
 @SuppressLint("ValidFragment")
 public class DialFragment extends Fragment {
+    @Bind(R.id.dialTv)
+    TextView dialTv;
     private String title = "";
+
     public DialFragment(String title) {
         super();
         this.title = title;
 
     }
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dial, null);
 
 
+        ButterKnife.bind(this, view);
+
+        dialTv.setText("Butter knift Test!!!");
         return view;
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
 }
